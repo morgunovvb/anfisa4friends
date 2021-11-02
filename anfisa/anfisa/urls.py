02fbbd0bdb1a0afef18a3295e6043516b72ce8d4
Вячеслав <74514540+morgunovvb@users.noreply.github.com>
2021-11-03 02:42:46 +0300

    Добавлены urls и view-функци

--- anfisa/anfisa/urls.py
+++ anfisa/anfisa/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 # anfisa/urls.py (главный файл url проекта)
-# По умолчанию в проект Django подключена система администрирования 
+# По умолчанию в проект Django подключена система администрирования
 from django.contrib import admin
 # Функция include позволит использовать path() из других файлов.
 # Импортируем!
-from django.urls import include, path 
+from django.urls import include, path
 
 urlpatterns = [    
     # Дорогой Джанго, если запрошена главная страница (''),
--- anfisa/ice_cream/views.py
+++ anfisa/ice_cream/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# ice_cream/views.py
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def ice_cream_list(request):
+    return HttpResponse('Список мороженого')
+
+
+# Страница с информацией об одном сорте мороженого;
+# view-функция принимает параметр pk из path()
+def ice_cream_detail(request, int:pk):
+    return HttpResponse(f'Мороженое номер {int:pk}')
