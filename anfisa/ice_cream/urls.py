0880f07e529a7b5cb3fe0cfbe03f1da7cfeda13f
Вячеслав <74514540+morgunovvb@users.noreply.github.com>
2021-12-24 19:34:46 +0300

    Anfisa v. 2.1

--- anfisa/ice_cream/urls.py
+++ anfisa/ice_cream/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     # Страница со списком сортов мороженого
     path('ice_cream/', views.ice_cream_list),
     # Отдельная страница с информацией о сорте мороженого
-    path('ice_cream/<pk>/', views.ice_cream_detail),
+    path('ice_cream/<int:pk>/', views.ice_cream_detail),
 ]
--- anfisa/ice_cream/views.py
+++ anfisa/ice_cream/views.py
@@ -1,18 +1,23 @@
 # ice_cream/views.py
 from django.http import HttpResponse
+# Импортируем загрузчик.
+from django.template import loader
+# Импортируем рендеринг
+from django.shortcuts import render
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
-
+def index(request):
+    template = 'ice_cream/index.html'
+    return render(request, template)
 
 # Страница со списком мороженого
 def ice_cream_list(request):
-    return HttpResponse('Список мороженого')
-
+    template = 'ice_cream/ice_cream_list.html'
+    return render(request, template)
 
 # Страница с информацией об одном сорте мороженого;
 # view-функция принимает параметр pk из path()
 def ice_cream_detail(request, pk):
-    return HttpResponse(f'Мороженое номер {pk}')
+    template = 'ice_cream/ice_cream_detail.html'
+    return render(request, template)
